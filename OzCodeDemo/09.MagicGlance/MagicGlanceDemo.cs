73505d5142add8eb557a6e2b29da0136873c919a
Dror Helper <dror.helper@gmail.com>
2015-12-29 10:49:59 +0200

    Added demos for Predict & magic-glance

---
+++ OzCodeDemo/09.MagicGlance/MagicGlanceDemo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using OzCodeDemo.DemoClasses.Customers;
+
+namespace OzCodeDemo._09.MagicGlance
+{
+    [Export(typeof(IOzCodeDemo))]
+    [ExportMetadata("Demo", "MagicGlance")]
+    public class MagicGlanceDemo : IOzCodeDemo
+    {
+        public void Start()
+        {
+            var customers = CustomersRepository.LoadCustomersFromDb().Take(10);
+
+            var expenses = customers.Sum(customer => CalculateExpenses(customer));
+
+            MessageBox.Show("Total cost for customers: " + expenses);
+        }
+
+        private static double CalculateExpenses(Customer customer)
+        {
+            Debugger.Break();
+
+            int retailerGiftPrice = IsElgibileForDiscount(customer) ? GetDiscountPrice() : GetRegularPrice();
+
+            int shippingCost;
+            if (int.TryParse(GetShippingCostFromConfig(), out shippingCost))
+            {
+                retailerGiftPrice += shippingCost;
+            }
+
+            AddVAT(ref retailerGiftPrice);
+
+            return retailerGiftPrice + GetExtraFreebiesCost(customer);
+        }
+
+        private static string GetShippingCostFromConfig()
+        {
+            return "404";
+        }
+
+        private static double GetExtraFreebiesCost(Customer customer)
+        {
+            return customer.GetYearlyEarnings(100) + 52;
+        }
+
+        private static int GetRegularPrice()
+        {
+            return 512;
+        }
+
+        private static int GetDiscountPrice()
+        {
+            return 256;
+        }
+
+        private static bool IsElgibileForDiscount(Customer customer)
+        {
+            return customer.Birthday.Month < 6;
+        }
+
+        private static void AddVAT(ref int currentGiftPrice)
+        {
+            currentGiftPrice = Convert.ToInt32(currentGiftPrice * 1.18);
+        }
+    }
+}
---
+++ OzCodeDemo/10.Predict/Predict.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Linq;
+using OzCodeDemo.DemoClasses.Customers;
+
+namespace OzCodeDemo._10.Predict
+{
+    [Export(typeof(IOzCodeDemo))]
+    [ExportMetadata("Demo", "Predict")]
+    public class Predict : IOzCodeDemo
+    {
+        const int MaxEmailLength = 250;
+
+        public void Start()
+        {
+            var customers = LoadCustomersFromDb();
+
+            Debugger.Break();
+
+            foreach (var customer in customers)
+            {
+                switch (customer.Gender)
+                {
+                    case Gender.Male:
+                        if (customer.Address.Country == "France" ||
+                            customer.Address.Country == "Canada" ||
+                            customer.GetYearlyEarnings(2014) > 10000 ||
+                            customer.CCType == "Visa")
+                        {
+                            SendGift(customer);
+                        }
+                        else 
+                        {
+                            SendThankYouLetter(customer);
+                        }
+                        break;
+                    case Gender.Female:
+                        SendGift(customer);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }          
+        }
+
+        private void SendThankYouLetter(Customer customer)
+        {
+            if (customer.EmailAddress.Length > MaxEmailLength)
+            {
+                SendThankYouLetterInternal(customer.EmailAddress);
+            }
+            else
+            {
+                Debug.Write("Email address too long!");
+            }
+        }
+
+        private void SendThankYouLetterInternal(string email)
+        {
+            // TODO :)
+        }
+
+        private void SendGift(Customer customer)
+        {
+            // TODO :)
+        }
+
+        private void SendBill(object customers)
+        {
+            // TODO :)
+        }
+
+        private static List<Customer> LoadCustomersFromDb()
+        {
+            var customers = CustomersRepository.LoadCustomersFromDb();
+
+            customers[5].Gender = Gender.Male;
+            customers[5].CCType = "MasterCard";
+            customers[5].Address.Country = "UK";
+            customers[5].EmailAddress = null;
+
+            return customers;
+        }
+    }
+}
